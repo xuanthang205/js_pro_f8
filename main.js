6c26f7f768a1d7e0281650890f105ffbd0cfc59c
쑤언 탕_Xuan Thang <thangdx@hivelab.com>
2025-12-23 11:39:08 +0700

    Todo list #2

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-const tasks = [
-  { title: "Design a website", completed: true },
-  { title: "Write blog post", completed: false },
-  { title: "Create marketing plan", completed: false }
-];
+const tasks = JSON.parse(localStorage.getItem("tasks")) ?? [];
 
 const taskList = document.getElementById("task-list");
 const todoForm = document.getElementById("todo-form");
 const todoInput = document.getElementById("todo-input");
+
+function escapeHtml(html) {
+  const div = document.createElement("div");
+  div.innerText = html;
+  return div.innerHTML;
+}
 
 function cleanUpSpaces(value) {
   const words = value
@@ -27,7 +29,7 @@ function renderTasks() {
   const html = tasks
     .map((task, index) => {
       return `<li class="task-item ${task.completed ? "completed" : ""}" data-task-index="${index}">
-                    <span class="task-title">${task.title}</span>
+                    <span class="task-title">${escapeHtml(task.title)}</span>
                     <div class="task-action">
                         <button class="task-btn edit">Sửa</button>
                         <button class="task-btn done">${task.completed ? "Chưa hoàn thành" : "Hoàn thành"}</button>
@@ -39,26 +41,53 @@ function renderTasks() {
   taskList.innerHTML = html;
 }
 
+function saveTasks() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function isDuplicateTask(newTitle, excludeIndex = -1) {
+  return tasks.some((task, index) => task.title.toLowerCase() === newTitle.toLowerCase() && index !== excludeIndex);
+}
+
 taskList.addEventListener("click", function (e) {
   const taskItem = e.target.closest(".task-item");
+  if (!taskItem) return;
   const taskIndex = +taskItem.dataset.taskIndex;
   const task = tasks[taskIndex];
 
-  if(e.target.closest(".edit")){
+  if (e.target.closest(".edit")) {
     let newTitle = prompt("Nhập tiêu đề công việc mới:", task.title);
 
-    if(newTitle === null) return;
+    if (newTitle === null) return;
 
     newTitle = cleanUpSpaces(newTitle);
 
-    if(!newTitle){
+    if (!newTitle) {
       alert("Tiêu đề công việc không được để trống.");
       return;
     }
+
+    if (isDuplicateTask(newTitle, taskIndex)) {
+      alert("Công việc đã tồn tại trong danh sách! Vui lòng nhập công việc khác."); // bam edit -> ok nhung van bao loi
+      todoInput.value = "";
+      return;
+    }
+
     task.title = newTitle;
     renderTasks();
+    saveTasks();
+  } else if (e.target.closest(".done")) {
+    task.completed = !task.completed;
+    renderTasks();
+    saveTasks();
+  } else if (e.target.closest(".delete")) {
+    if (confirm(`Bạn có chắc chắn muốn xóa công việc ${task.title} không?`)) {
+      tasks.splice(taskIndex, 1);
+      renderTasks();
+      saveTasks();
+    }
   }
-})
+});
 
 todoForm.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -66,22 +95,17 @@ todoForm.addEventListener("submit", function (e) {
   if (!value) {
     alert("Công việc không thể trống, vui lòng nhập một công việc!");
     todoInput.value = "";
-    console.log(value);
-
     return;
   }
 
-  const isDuplicate = tasks.some((task) => {
-    return task.title.toLowerCase() === value.toLowerCase();
-  });
-
-  if (isDuplicate) {
+  if (isDuplicateTask(value)) {
     alert("Công việc đã tồn tại trong danh sách! Vui lòng nhập công việc khác.");
     todoInput.value = "";
     return;
   }
   tasks.push({ title: value, completed: false });
   renderTasks();
+  saveTasks();
   todoInput.value = "";
 });
 
